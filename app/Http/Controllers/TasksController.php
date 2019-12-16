f1166da8e1da1a7bfda55db448ce0d23c51524af
tomohirohasegawa2 <tmhr.hsgw.w@gmail.com>
2019-12-16 13:11:01 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -90,7 +95,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
